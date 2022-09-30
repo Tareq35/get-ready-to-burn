bccda2e2aba8b7523b1bb8cc5e6ddde1dba57d9c
Tareq35 <a.t.tariqulislam@gmail.com>
2022-09-30 20:28:10 +0600

    profile person data added

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import './List.css';
+import profile from "../../images/profile.jpg"
 
 const List = (props) => {
     const { list } = props;
 
     let total = 0;
-    for(const exercise of list){
+    for (const exercise of list) {
         total = total + exercise.timeRequired;
     }
 
     return (
         <div className='list'>
-            <div>
-            <h5>Add A Break</h5>
-            
+            <div className='profile-content'>
+                <div>
+                    <img className='profile-img' src={profile} alt="" />
+                </div>
+                <div>
+                    <h4>Tareq Islam</h4>
+                    <p><small>Dhaka, Bangladesh</small></p>
+                </div>
             </div>
-            <h5>Exercise Details</h5>
+            <div className='person-data-container'>
+                <div>
+                    <p><span className='person-data'>82</span> <small>kg</small></p>
+                    <p>Weight</p>
+                </div>
+                <div>
+                    <p><span className='person-data'>5.9</span> <small>feet</small></p>
+                    <p>Height</p>
+                </div>
+                <div>
+                    <p><span className='person-data'>25</span> <small>years</small></p>
+                    <p>Age</p>
+                </div>
+
+            </div>
+
+            <div>
+                <h4>Add A Break</h4>
+                <button className='break-btn'><p>2 min</p></button>
+                <button className='break-btn'><p>5 min</p></button>
+                <button className='break-btn'><p>8 min</p></button>
+                <button className='break-btn'><p>10 min</p></button>
+                <button className='break-btn'><p>15 min</p></button>
+
+            </div>
+            <h4>Exercise Details</h4>
             <p>Exercise time: {total} min</p>
             <p>Break time: </p>
             <button className='btn-activity'><p>Activity Completed</p></button>
